35f7f4f5b3708059fc9d1f13c8f0c86450ece75c
Bharat231987 <45390952+Bharat231987@users.noreply.github.com>
2018-12-22 23:55:27 +0530

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,14 +1,14 @@
-
+# shinyUI() and define the appearance of our app
 shinyUI(
-  fluidPage(# theme = shinytheme("darkly"),
+  fluidPage(# theme = shinytheme("darkly"),      #fluidPage() defines a dynamic application layout enabling the app to automatically fit a screen size
     
   
-    titlePanel(h1("NLP using UDPipe R Package", 
+    titlePanel(h1("NLP using UDPipe R Package",                # titlePanel() creates a panel containing an application title.
                    style = "font-family: 'Calibri', cursive;
                    font-style:italic;
                    color: #d76336;")),
-    sidebarLayout(
-      sidebarPanel(
+    sidebarLayout(                                             # creates a layout with a sidebar and main area
+      sidebarPanel(                                            # sidebarPanel() creates a sidebar panel containing input controls
         fileInput("file1",label = h3("Upload Data (Any text file)")),
         fileInput("file2",label = h3("Upload UDPipe Language Model")),
         
@@ -22,10 +22,10 @@ shinyUI(
                                            ),
                            selected = c("JJ","NN","NNP"))
       ),
-      mainPanel(
-        tabsetPanel(type = "tabs",
+      mainPanel(                       # mainPanel() creates a main panel containing output elements, typically structured in tabs
+        tabsetPanel(type = "tabs",     # tabsetPanel() creates a set of tabs with tabPanel elements  
                     
-                    tabPanel(h4("Overview"),
+                    tabPanel(h4("Overview"),    #tabPanel() creates a tab panel that can be included within a tabsetPanel.
                              h2(p("NLP in R")),
                              (p("The purpose of this App is to showcase the NLP capabilties of R using R UDPipe package.
                                UDPipe provides the standard NLP functionalities of tagging, parsing and dependency evaluations - all within R",align = "justify")),
