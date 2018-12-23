390cb130d03b9657fa1af56149566ec1a0a7f65e
Nitin Soneji <nisoneji@microsoft.com>
2018-12-23 22:06:18 +0530

    Add files via upload

--- server.R
+++ server.R
@@ -1,7 +1,14 @@
+# Bharat Batra | 11910105 | bharat_batra_cbas2019@isb.edu
+# Nitin Soneji | 11810062 | nitin_soneji_cba2019w@isb.edu
+# Rupesh Lad   | 11810031 | rupesh_lad_cba2019w@isb.edu
+
+#TABA Assignment
+#Problem 1: Building a Shiny App around the UDPipe NLP workflow
 
 options(shiny.maxRequestSize = 30*1024^2)
 shinyServer(function(input,output){  # server function for input and output 
   
+ 
   Dataset <- reactive({              # reactive function to dynamically capture the data
     if(is.null(input$file1)){
       return(NULL)
@@ -19,11 +26,13 @@ shinyServer(function(input,output){  # server function for input and output
   })
   
   model <- reactive({                                #loading selected model
-    if(is.null(input$file2)){ return (NULL)}
+    if(is.null(input$file2)){ 
+           return (NULL)}
     else {
-    udpipe_model = udpipe_load_model(input$file2$datapath)
-    return(udpipe_model)
-    }
+          udpipe_model = udpipe_load_model(input$file2$datapath)
+          return(udpipe_model)
+              
+        }
   })
   
   annotated_df = reactive({        #creating annotated document using udpipe     
@@ -33,36 +42,43 @@ shinyServer(function(input,output){  # server function for input and output
     return(x)
   })
   
-  
   output$annotatedOutput = renderDataTable({   # creating data table of annotated data frame
     if(is.null(input$file1)){
+      textOutput("No textfile is uploaded.")
       return(NULL)
     }
     else{
+      #Adding progress bar to show user that it is processing
+      withProgress(message = "Annotated Document processing in progress...",{
       out = annotated_df()[,-4]
       out = out %>% subset(.,xpos %in% input$xpos)
+      incProgress(amount = 1, message ="Completed" )
       return(out)
+      })
     }
   })
   
   plotname <- reactive(    # For loop for seeting the subtitle plot dynamically
     {  
-  plotname = NULL
-  
-  for ( i in 1:length(input$xpos))
-  {
-    plotname = paste(plotname,input$xpos[i]) 
-    
-  }
-  
-  return (plotname)
+      plotname = NULL
+      
+      
+      for ( i in 1:length(input$xpos))
+      {
+        plotname = paste(plotname,input$xpos[i]) 
+        
+      }
+      return (plotname)
     }
   )
+  
   output$cooccurence = renderPlot({    # render coocurence plot
     if(is.null(input$file1)){
       return(NULL)
     }
     else{
+      #Adding progress bar to show user that it is processing
+      withProgress(message = "Co-occurances graph preparation in progress...", {
       text_cooc <- cooccurrence(
         x = subset(annotated_df(),xpos %in% input$xpos),
         term = "lemma",
@@ -80,8 +96,11 @@ shinyServer(function(input,output){  # server function for input and output
         theme(legend.position = "none") +
         
         labs(title = "Cooccurence plot", subtitle = plotname() )
-    
+      
+       })
+      
     }
   }) 
- 
+  
 })
+  
